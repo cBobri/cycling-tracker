2d9248fedfe22e79d4678d0512cda51781677c73
tadejrebernjak <tadej.rebernjak2@gmail.com>
2024-05-21 14:14:24 +0200

    Moved recording to a separate page

--- cycling-recorder/app/index.tsx
+++ cycling-recorder/app/index.tsx
@@ -1,75 +1,12 @@
-import { Text, View, StyleSheet, Button } from "react-native";
-import React, { useEffect, useState } from "react";
-import MagnitudeSensors from "../components/MagnitudeSensors";
-import { haversineDistance } from "@/helpers/haversineDistance";
-import { getLocation } from "@/helpers/getLocation";
+import { Text, View, StyleSheet } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
 
 export default function Index() {
-    const [isRecording, setIsRecording] = useState<boolean>(false);
-    const [magnitude, setMagnitude] = useState<any>(null);
-    const [GPS, setGPS] = useState<any>(null);
-    const [distance, setDistance] = useState<number>(0);
-    const [recordingStart, setRecordingStart] = useState<number | null>(null);
-
-    useEffect(() => {
-        if (GPS && magnitude) {
-            const data = {
-                routeID: "lol",
-                timestamp: Date.now(),
-                gps: GPS,
-                magnitude,
-            };
-
-            console.log("-----Data to send");
-            console.log(JSON.stringify(data));
-        }
-    }, [GPS]);
-
-    useEffect(() => {
-        if (isRecording) setRecordingStart(Date.now());
-
-        if (GPS == null) {
-            updateGPS();
-        }
-
-        const interval = setInterval(() => {
-            if (isRecording) updateGPS();
-        }, 10000);
-
-        return () => {
-            clearInterval(interval);
-        };
-    }, [isRecording]);
-
-    const toggleRecording = () => {
-        setIsRecording((prevState: boolean) => !prevState);
-    };
-
-    const handleMagnitudeData = (data: any) => {
-        setMagnitude(data);
-    };
-
-    const updateGPS = async () => {
-        const data = await getLocation();
-        if (GPS) setDistance(distance + haversineDistance(data, GPS));
-        setGPS(data);
-    };
-
     return (
         <View style={styles.container}>
-            <Button
-                title={isRecording ? "Stop Recording" : "Start Recording"}
-                onPress={toggleRecording}
-            />
-            {isRecording && (
-                <>
-                    <MagnitudeSensors handleNewResult={handleMagnitudeData} />
-                    <Text style={styles.text}>Distance: {distance} meters</Text>
-                    <Text style={styles.text}>
-                        Time: {(Date.now() - recordingStart!) / 1000} seconds
-                    </Text>
-                </>
-            )}
+            <Text>Edit app/index.tsx to edit this screen</Text>
+            <Link href="/record">Record</Link>
         </View>
     );
 }
@@ -80,9 +17,6 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "#fff",
-    },
-    text: {
-        fontSize: 18,
-        margin: 10,
+        gap: 50,
     },
 });
--- cycling-recorder/components/MagnitudeSensors.tsx
+++ cycling-recorder/components/MagnitudeSensors.tsx
@@ -38,10 +38,12 @@ export default function MagnitudeSensors({ handleNewResult }: Props) {
     const [avgMagnitude, setAvgMagnitude] = useState(0);
     const [shakingLevel, setShakingLevel] = useState(0);
     const [dataArray, setDataArray] = useState([]);
+    const [previousUpdate, setPreviousUpdate] = useState<number>(Date.now());
 
     useEffect(() => {
-        if (avgMagnitude != 0) {
+        if (avgMagnitude != 0 && Date.now() - previousUpdate > 1000) {
             handleNewResult({ value: avgMagnitude, level: shakingLevel });
+            setPreviousUpdate(Date.now());
         }
     }, [avgMagnitude]);
 
