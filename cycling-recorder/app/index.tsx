aa14d3ad05bf68ff38d47ca1c00f1c5b02fee13a
tadejrebernjak <tadej.rebernjak2@gmail.com>
2024-05-21 11:52:18 +0200

    Reading data from accelerometer and gyroscope

--- cycling-recorder/app/index.tsx
+++ cycling-recorder/app/index.tsx
@@ -1,22 +1,21 @@
 import { Text, View, StyleSheet } from "react-native";
-import React from 'react';
-import AccelerometerSensor from '../components/AccelerometerSensor';
-
+import React from "react";
+import MagnitudeSensors from "../components/MagnitudeSensors";
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <AccelerometerSensor />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text>Edit app/index.tsx to edit this screen.</Text>
+            <MagnitudeSensors />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+    },
 });
