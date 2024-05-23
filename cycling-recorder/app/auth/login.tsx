ae3f56ae45b0563e6d46ad185b967a790e93d7e6
zalozniktine59 <tine.zaloznik@student.um.si>
2024-05-23 18:11:38 +0200

    Dodan izgled za auth

--- cycling-recorder/app/auth/login.tsx
+++ cycling-recorder/app/auth/login.tsx
@@ -1,6 +1,43 @@
-import { Text } from 'react-native';
-import { Link } from 'expo-router';
+import React, { useState } from 'react';
+import { View, Text } from 'react-native';
+import { TextInput, Button } from 'react-native-paper';
+import authStyles from '../../styles/authStyle';
 
-export default function Login() {
-  return <Text>Login</Text>;
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () =>{
+        
+    };
+
+    return (
+        <View style={authStyles.container}>
+            <TextInput
+                label = "Username"
+                value = {username}
+                onChangeText = {setUsername}
+                style = {authStyles.input}
+                autoCapitalize='none'
+                theme={{ roundness: 10 }}
+            />
+            <TextInput
+                label= "Password"
+                value = {password}
+                onChangeText = {setPassword}
+                style = {authStyles.input}
+                secureTextEntry
+                theme={{ roundness: 10 }}
+            />
+            <Button mode="contained" onPress={handleLogin} style={authStyles.button}>
+                <Text style={authStyles.buttonText}>Login</Text>
+            </Button>
+        </View>
+
+    )
+    
+    
+    ;
 }
+
+export default Login;
--- cycling-recorder/app/auth/register.tsx
+++ cycling-recorder/app/auth/register.tsx
@@ -1,5 +1,66 @@
-import { Text } from 'react-native';
+import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { TextInput, Button } from 'react-native-paper';
+import authStyles from '../../styles/authStyle';
+import axios from 'axios';
 
-export default function Register() {
-  return <Text>Register</Text>;
+
+const Register = () => {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleRegister =  async () => {
+        if (email === '' || username === '' || password === '') {
+            alert('Please fill all the fields');
+            return;
+        }
+        const res = await axios.post('http://192.168.31.210:8081/users/register', {
+            email,
+            username,
+            password,
+        });
+        if (res.data.error) {
+            alert(res.data.error);
+            return;
+        }
+        if (res.status === 200) {
+            alert('User registered successfully');
+        }
+    };
+    return (
+        <View style={authStyles.container}>
+          <TextInput
+            label="Email"
+            value={email}
+            onChangeText={setEmail}
+            style={authStyles.input}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            theme={{ roundness: 10 }}
+          />
+          <TextInput
+            label="Username"
+            value={username}
+            onChangeText={setUsername}
+            style={authStyles.input}
+            autoCapitalize="none"
+            theme={{ roundness: 10 }}
+          />
+          <TextInput
+            label="Password"
+            value={password}
+            onChangeText={setPassword}
+            style={authStyles.input}
+            secureTextEntry
+            theme={{ roundness: 10 }}
+          />
+          <Button mode="contained" onPress={handleRegister} style={authStyles.button}>
+            <Text style={authStyles.buttonText}>Register</Text>
+          </Button>
+        </View>
+      );
+
 }
+
+export default Register;
