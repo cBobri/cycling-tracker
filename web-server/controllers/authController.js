38b195cc6c3a09006010043931f4d6218dd0ab50
Tadej Rebernjak <tadej.rebernjak2@gmail.com>
2024-06-05 18:54:30 +0200

    Changed notification message and api urls

--- web-server/controllers/authController.js
+++ web-server/controllers/authController.js
@@ -1,18 +1,1 @@
-const { sendNotifications } = require("../helpers/notifications");
-
-module.exports = {
-    sendNotification: async (req, res, next) => {
-        try {
-            const count = await sendNotifications(req.user._id);
-
-            return res
-                .status(200)
-                .json({ message: `Sent ${count} notifications` });
-        } catch (err) {
-            console.log(err);
-            const error = new Error("Failed to send notification");
-            error.status = 500;
-            return next(error);
-        }
-    },
-};
+module.exports = {};
--- web-server/routes/authRouter.js
+++ web-server/routes/authRouter.js
@@ -2,13 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const { requireUser, checkUser } = require("../controllers/usersController");
-const { sendNotification } = require("../controllers/authController");
+const { sendTestNotification } = require("../controllers/authController");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
     res.send("respond with a resource");
 });
-
-router.post("/send-notification", checkUser, requireUser, sendNotification);
 
 module.exports = router;
