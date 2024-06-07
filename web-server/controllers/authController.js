3f4acfa55cfb4e3eeb13e12e4ce163ecd319fb6f
Tadej Rebernjak <tadej.rebernjak2@gmail.com>
2024-06-07 15:37:59 +0200

    Added auth check endpoint

--- web-server/controllers/authController.js
+++ web-server/controllers/authController.js
@@ -35,6 +35,17 @@ module.exports = {
                 return next(error);
             }
 
+            const authRequest = await AuthRequestModel.findOne({
+                user: user._id,
+                finished_processing: false,
+            });
+
+            if (!authRequest) {
+                const error = new Error("No auth request present");
+                error.status = 404;
+                return next(error);
+            }
+
             res.setHeader("Content-Type", "text/event-stream");
             res.setHeader("Cache-Control", "no-cache");
             res.setHeader("Connection", "keep-alive");
@@ -66,9 +77,12 @@ module.exports = {
 
     respondAndCloseConnection: async (req, res, next) => {
         try {
+            const fiveMinutesAgo = new Date(Date.now() - 5 * 60 * 1000); // 5 minutes ago
+
             const authRequest = await AuthRequestModel.findOne({
                 user: req.user._id,
                 finished_processing: false,
+                createdAt: { $gte: fiveMinutesAgo },
             });
 
             if (!authRequest) {
@@ -99,6 +113,24 @@ module.exports = {
         } catch (err) {
             console.log(err);
             const error = new Error("Error when closing connection");
+            error.status = 500;
+            return next(error);
+        }
+    },
+
+    checkForAuthRequest: async (req, res, next) => {
+        try {
+            const exists = await AuthRequestModel.exists({
+                user: req.user._id,
+                finished_processing: false,
+            });
+
+            return res.status(200).json({ requestExists: !!exists });
+        } catch (err) {
+            console.log(err);
+            const error = new Error(
+                "Error when checking for authentication request"
+            );
             error.status = 500;
             return next(error);
         }
--- web-server/routes/authRouter.js
+++ web-server/routes/authRouter.js
@@ -5,9 +5,11 @@ const { requireUser, checkUser } = require("../controllers/usersController");
 const {
     establishConnection,
     respondAndCloseConnection,
+    checkForAuthRequest,
 } = require("../controllers/authController");
 
 router.get("/listen/:userId", establishConnection);
+router.get("/check", checkUser, requireUser, checkForAuthRequest);
 router.post("/finish", checkUser, requireUser, respondAndCloseConnection);
 
 module.exports = router;
