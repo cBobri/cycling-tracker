3f17e6ac217ecae736e37e786dbb4e562d8afe6b
Tadej Rebernjak <tadej.rebernjak2@gmail.com>
2025-01-17 14:39:56 +0100

    Update route model

--- web-server/models/routeModel.js
+++ web-server/models/routeModel.js
@@ -1,62 +1,49 @@
 const mongoose = require("mongoose");
 
 const statsSchema = new mongoose.Schema({
-    avgSpeed: Number,
-    maxSpeed: Number,
-    elevation: Number,
-    distance: Number,
-    travelTime: Number,
-    power: Number,
-    powerRatio: Number,
-    energy: Number,
+  avgSpeed: Number,
+  maxSpeed: Number,
+  elevation: Number,
+  distance: Number,
+  travelTime: Number,
+  power: Number,
+  powerRatio: Number,
+  energy: Number,
 });
 
 const routeSchema = new mongoose.Schema({
-    data: [
-        {
-            gps: {
-                latitude: Number,
-                longitude: Number,
-                altitude: Number,
-            },
-            magnitude: {
-                value: Number,
-                level: Number,
-                data: [
-                    {
-                        ax: Number,
-                        ay: Number,
-                        az: Number,
-                        gx: Number,
-                        gy: Number,
-                        gz: Number,
-                    },
-                ],
-            },
-            moving: Boolean,
-            timestamp: { type: Date },
-        },
-    ],
-    isProcessed: Boolean,
-    isPublic: Boolean,
-    title: String,
-    description: String,
-    bikeWeight: Number,
-    cyclistWeight: Number,
-    stats: statsSchema,
-    segments: [statsSchema],
-    percentageStats: [statsSchema],
-    centerCoordinates: {
+  data: [
+    {
+      gps: {
         latitude: Number,
         longitude: Number,
+        altitude: Number,
+      },
+      moving: Boolean,
+      timestamp: { type: Date },
     },
-    proIndex: Number,
-    winnerIndex: Number,
-    referencedRaces: [{ type: mongoose.Schema.Types.ObjectId, ref: "race" }],
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
-    recordingStart: Date,
-    recordingEnd: Date,
-    createdAt: { type: Date, default: Date.now },
+  ],
+  magnitudeData: { type: [Number], default: [] },
+  isProcessed: Boolean,
+  isPublic: Boolean,
+  title: String,
+  description: String,
+  bikeWeight: Number,
+  cyclistWeight: Number,
+  stats: statsSchema,
+  segments: [statsSchema],
+  percentageStats: [statsSchema],
+  centerCoordinates: {
+    latitude: Number,
+    longitude: Number,
+  },
+  proIndex: Number,
+  winnerIndex: Number,
+  referencedRaces: [{ type: mongoose.Schema.Types.ObjectId, ref: "race" }],
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+  recordingStart: Date,
+  recordingEnd: Date,
+  createdAt: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model("route", routeSchema);
